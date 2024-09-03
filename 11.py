3f9c55e7fc1f43f542b7961ac69113609dd02e67
wind <windwillwin66@gmail.com>
2024-09-03 14:45:50 +0800

    add end time

--- 11.py
+++ 11.py
@@ -4,6 +4,8 @@ start_time = time.time()
 time.sleep(4)
 time.sleep(2)
 print(f'{start_time} is start time')
+
 end_time = time.time()
 print(f'use time id {end_time-start_time}')
+print(f'{end_time} is end  time')
 print('11')
